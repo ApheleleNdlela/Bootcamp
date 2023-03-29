9775b251613e1703998dd269192f448864364656
Aphelele Ndlela <aphelelendlela95@gmail.com>
2023-03-29 16:01:33 +0200

    fundamentalsJs

---
+++ Jsfundamentals/fundamentals.js
@@ -0,0 +1,92 @@
+/*var a = 1;
+var a = 10;
+
+let a = 1;
+let a = 10;
+console.log(a);
+
+let a = 1;
+let b = 2;
+let c = a + b;
+console.log(c);
+
+let a = 10;
+let b = 3;
+let c = a % b;
+console.log(c)
+
+let a = "10";
+let b = 10;
+if(a != b){
+    console.log('true');
+} else {console.log("false");
+}
+
+let d = 10;
+let c = 10;
+let a = 10;
+let b = 20;
+
+if(a == b && c == d) 
+{console.log('true')}
+ else 
+ {console.log('false')}
+
+
+ [temporary variables]
+
+let a = 24;
+let b = 28;
+let c;
+
+c = a;
+a = b;
+b = c;
+
+console.log(a);
+console.log(b); 
+
+let a = 24;
+let b = 28;
+
+[a, b] = [28, 24, 20]
+
+console.log(a);
+console.log(b);
+
+
+
+let a = -2
+
+if (a >10 ) {console.log('greater than a decade')} 
+else if (a == 10) 
+{ console.log('it is a decade')} else if (a < 10 && a >= 0 ) 
+{console.log('it is lesser than a decade')} else {
+console.log('we can not have a negative year')}
+*/
+
+//Excercise number 2: declare 3 variables (numbers) and return a maximum number
+
+/*let a = 2;
+let b = 4;
+let c = 6;
+
+if (b > a) {console.log('true')} else if 
+(b > c) {console.log('false')} else if
+(b>a && c > b) {console.log('maximum number')}*/
+
+let a = 2;
+let b = 4;
+let c = 6;
+
+/*if (b>a && b>c) {console.log(b)} 
+else if(b<a && a>c){console.log(a)}
+else if (c>a && c>b) {console.log(c)}*/
+
+
+/*if (a>b && a>c) {console.log(c)}
+if (b<a){console.log('false')} 
+else if (c>b) console.log('true')*/
+
+//corrections
+
