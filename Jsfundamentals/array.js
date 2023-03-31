f069dd97d74fb90f0f6305863d0c0dd3515d55f9
Aphelele Ndlela <aphelelendlela95@gmail.com>
2023-03-31 13:45:06 +0200

    arrays

---
+++ Jsfundamentals/array.js
@@ -0,0 +1,13 @@
+let arrd1 = [2,4,6,8,9,23,54,12]
+
+// pop - removes last elememt;
+// push - adds last elememt;
+// shift - removes first elememt
+// unshift - adds first elememt
+
+arrd1.pop()
+arrd1.push(14)
+arrd1.shift(2)
+arrd1.unshift(3)
+
+console.log(arrd1)
--- Jsfundamentals/fundamentals.js
+++ Jsfundamentals/fundamentals.js
@@ -1,145 +1,153 @@
-// // /*var a = 1;
-// // var a = 10;
+// var a = 1;
+// var a = 10;
 
-// // let a = 1;
-// // let a = 10;
-// // console.log(a);
+// let a = 1;
+// let a = 10;
+// console.log(a);
 
-// // let a = 1;
-// // let b = 2;
-// // let c = a + b;
-// // console.log(c);
+// let a = 1;
+// let b = 2;
+// let c = a + b;
+// console.log(c);
 
-// // let a = 10;
-// // let b = 3;
-// // let c = a % b;
-// // console.log(c)
+// let a = 10;
+// let b = 3;
+// let c = a % b;
+// console.log(c)
 
-// // let a = "10";
-// // let b = 10;
-// // if(a != b){
-// //     console.log('true');
-// // } else {console.log("false");
-// // }
+// let a = "10";
+// let b = 10;
+// if(a != b){
+//     console.log('true');
+// } else {console.log("false");
+// }
 
-// // let d = 10;
-// // let c = 10;
-// // let a = 10;
-// // let b = 20;
+// let d = 10;
+// let c = 10;
+// let a = 10;
+// let b = 20;
 
-// // if(a == b && c == d) 
-// // {console.log('true')}
-// //  else 
-// //  {console.log('false')}
-
-
-// //  [temporary variables]
-
-// // let a = 24;
-// // let b = 28;
-// // let c;
-
-// // c = a;
-// // a = b;
-// // b = c;
-
-// // console.log(a);
-// // console.log(b); 
-
-// // let a = 24;
-// // let b = 28;
-
-// // [a, b] = [28, 24, 20]
-
-// // console.log(a);
-// // console.log(b);
+// if(a == b && c == d) 
+// {console.log('true')}
+//  else 
+//  {console.log('false')}
 
 
+//  [temporary variables]
 
-// // let a = -2
+// let a = 24;
+// let b = 28;
+// let c;
 
-// // if (a >10 ) {console.log('greater than a decade')} 
-// // else if (a == 10) 
-// // { console.log('it is a decade')} else if (a < 10 && a >= 0 ) 
-// // {console.log('it is lesser than a decade')} else {
-// // console.log('we can not have a negative year')}
-// // */
+// c = a;
+// a = b;
+// b = c;
 
-// // //Excercise number 2: declare 3 variables (numbers) and return a maximum number
+// console.log(a);
+// console.log(b); 
 
-// // /*let a = 2;
-// // let b = 4;
-// // let c = 6;
+// let a = 24;
+// let b = 28;
+
+// [a, b] = [28, 24, 20]
+
+// console.log(a);
+// console.log(b);
 
 
+
+// let a = -2
+
+// if (a >10 ) {console.log('greater than a decade')} 
+// else if (a == 10) 
+// { console.log('it is a decade')} else if (a < 10 && a >= 0 ) 
+// {console.log('it is lesser than a decade')} else {
+// console.log('we can not have a negative year')}
+
+
+// Excercise number 2: declare 3 variables (numbers) and return a maximum number
+
+// let a = 2;
+// let b = 4;
+// let c = 6;
 
 
 
 
 
-// // if (b > a) {console.log('true')} else if 
-// // (b > c) {console.log('false')} else if
-// // (b>a && c > b) {console.log('maximum number')}*/
-
-// // // let a = 2;
-// // // let b = 4;
-// // // let c = 6;
-
-// // /*if (b>a && b>c) {console.log(b)} 
-// // else if(b<a && a>c){console.log(a)}
-// // else if (c>a && c>b) {console.log(c)}*/
 
 
-// // /*if (a>b && a>c) {console.log(c)}
-// // if (b<a){console.log('false')} 
-// // else if (c>b) console.log('true')*/
+// if (b > a) {console.log('true')} else if 
+// (b > c) {console.log('false')} else if
+// (b>a && c > b) {console.log('maximum number')}*/
 
-// // //corrections
+// let a = 2;
+// let b = 4;
+// let c = 6;
 
-// // // let x = 5;
-
-// // // if(x % 2 == 0) {
-// // //     console.log('even number')
-// // // }
-// // // else {console.log('Odd number')
-
-
-// // // let y = -12
+// if (b>a && b>c) {console.log(b)} 
+// else if(b<a && a>c){console.log(a)}
+// else if (c>a && c>b) {console.log(c)}*/
 
 
-function y (){
-     let y = document.getElementById("evennumber").value
+// if (a>b && a>c) {console.log(c)}
+// if (b<a){console.log('false')} 
+// else if (c>b) console.log('true')*/
 
-    if (y % 2 == 0) {
-     alert(y +" "+ "is an even number")
+// corrections
 
-    } else { 
-    alert(y +" "+ "odd number")
-    }
-}
+// let x = 5;
+
+// if(x % 2 == 0) {
+// console.log('even number')
+// }
+// else {console.log('Odd number')
 
 
-// // // Array -->
+// let y = -12
+
+
+// function y (){
+//      let y = document.getElementById("evennumber").value
+
+//     if (y % 2 == 0) {
+//      alert(y +" "+ "is an even number")
+
+//     } else { 
+//     alert(y +" "+ "odd number")
+//     }
+// }
+
+
+// Array -->
 
 // let numbers = [2,9,11,7,5,3,4,8,15]
 
-// // let x = [2,9,11,7,5,3,4,8,15]
-// // // // for(let y = 0; y <= x.length; y++){
+// let x = [2,9,11,7,5,3,4,8,15]
+// for(let y = 0; y <= x.length; y++){
 
-// // // //     if(x[y] % 2 === 0 )
-// // // //     console.log(x[y])
-// // // }
+// if(x[y] % 2 === 0 )
+// console.log(x[y])
 
 // for(let i = 0; i <= numbers.length; i++ ){
-//     if (numbers[i] % 2 === 0)
-//     console.log(numbers[i])
+// if (numbers[i] % 2 === 0)
+// console.log(numbers[i])
 // }
 
-// // let i = 0;
-// // while(i < 10) {
-// //     console.log(i) 
-// //     i++
-// // }
+// let i = 0;
+// while(i < 10) {
+// console.log(i); i++
+// }
 
 
-// for()
+// let fnames = ["Aphelele", "Yamkela","Bulela","Liya"]
+
+
+// for(i = 0; i <= fnames.length; i++) {
+//     if(fnames[i])
+//    console.log(fnames[i][0])
+// }
+
+
+// let arrd = [[0,2,4],[1,3,5],[3,6,9],[7,8,10]]
+// let number6  = arrd[2][1]
