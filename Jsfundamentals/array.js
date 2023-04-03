ff4c3db91341fb0db5e6bdbd8c0e51b8e349553d
Aphelele Ndlela <aphelelendlela95@gmail.com>
2023-04-03 16:17:07 +0200

    fundamentals of functions

--- Jsfundamentals/array.js
+++ Jsfundamentals/array.js
@@ -1,4 +1,4 @@
-let arrd1 = [2,4,6,8,9,23,54,12]
+// let arrd1 = [2,4,6,8,9,23,54,12]
 
 // pop - removes last elememt;
 // push - adds last elememt;
@@ -11,3 +11,102 @@ let arrd1 = [2,4,6,8,9,23,54,12]
 // arrd1.unshift(3)
 
 // console.log(arrd1)
+
+
+// let numbers = [-3, -100, 10, 4, 6, 7, 20, 1]
+// numbers.sort((a,b) => a-b);
+// let minnummbers = numbers[0]
+
+
+// console.log(numbers)
+
+// let letters = ['L', 'U', 'P', 'H', 'A', 'W', 'Z', 'I']
+// letters.sort();
+// console.log(letters);
+
+// let numbers = [];
+
+
+// function maxAndMin(numbers) {
+
+//     let obj = {}
+//     numbers.sort((a,b) => a-b )
+//     obj["minNumber"] = numbers[0]
+//     obj["maxNumber"] = numbers[numbers.length -1]
+
+// console.log(obj)
+// return obj
+// }
+// console.log(typeof[0])
+// maxAndMin([1, 23, 15, 61, 17, 9, 101, 11])
+// let firstApp = maxAndMin([])
+// console.log(firstApp)
+
+
+// let fName = "LUPHAWU"
+
+// let splitName = fName.split("")
+
+// let reversedname = splitName.reverse()
+// let joinedname = reversedname.join("")
+
+//  console.log(reversedname);
+
+// let fname = "LUPHAWU"
+
+// let reversedname = "";
+
+// reversedname = fname.split("").reverse().join("")
+
+
+// console.log(reversedname)
+
+// for(let i = 0; i < fname.length -1; i++)
+
+// console.log(fname[i] + reversedname)
+
+// function strReversed (text) {
+
+//     let reversed = "";
+//     for (let i = 0; i < text.length, i ++;){
+//     reversed = text[i] + reversed;
+// }
+// console.log(reversed)
+// }
+// strReversed();
+// strReversed("java script is super easy");
+
+// function financicialCAlc(hR, hours, days,pci = 1.00) {
+
+// let salary = hR * hours * days * pci
+// console.log(salary)
+// }
+// financicialCAlc (95, 7, 20, 1.2)
+// financicialCAlc(95 * 7) this will return undefined because there is one parameter missing
+
+// function emailGen (fname = "Jeremy", Lname = "Van-Wyk") {
+//     let email = fname + "." + Lname + "@gsb.co.za"
+//     return email.toLowerCase()
+
+// }
+// console.log(emailGen())
+
+//     return fname + "." + Lname + "@gsb.co.za"
+// }
+//     console.log(emailGen)
+
+// function emailGen (fname = "Jeremy", Lname = "Van-Wyk"){
+
+//     console.log(fname + "." + Lname + "@gsb.co.za")
+// }
+// emailGen()
+
+
+let number1 = [1, 3, 5, 7]
+let number2 = [0, 2, 4, 6]
+let number3 = [...number1, ...number2]
+let number4 = number1.concat(number2)
+
+number3.sort((a,b) =>a-b)
+
+console.log("This is number3", number3)
