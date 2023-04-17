0f220dcf1bd279e4b84732d6f1a4afaa3d9a27e1
Aphelele Ndlela <aphelelendlela95@gmail.com>
2023-04-17 17:05:19 +0200

    styled Work

--- AgeCalculator/scripts.js
+++ AgeCalculator/scripts.js
@@ -1,27 +1,62 @@
-function AgeCalc (currentYr,bornYr){
+// function AgeCalc (currentYr,bornYr){
     
-    let Age = currentYr - bornYr
+//     let Age = currentYr - bornYr
 
-    if(Age < 0) {
-        console.log("Age can not be a negative number")
-    }  else if(!currentYr,!bornYr) {                // }  else if(isNaN(currentYr,bornYr)) {               
+//     if(Age < 0) {
+//         console.log("Age can not be a negative number")
+//     }  else if(!currentYr,!bornYr) {                // }  else if(isNaN(currentYr,bornYr)) {               
    
-        console.log("please enter your bornYr")
-    } else
-    console.log(Age)
-}
-AgeCalc()
+//         console.log("please enter your bornYr")
+//     } else
+//     console.log(Age)
+// }
+// AgeCalc()
 
 
-// function AgeCalc(bornYr) {
+
+
+// function AgeCalc() {
+//     let bornYr = document.getElementById("EnterAge").value
 //     let x = 2023 - bornYr
 //     if (bornYr < 0){
-//         console.log("Age can not be a negative")
+//         alert("Age can not be a negative")
 //     } else if (!bornYr) {
-//         console.log("please enter your born year")
+//         alert("please enter your born year")
 //     } else {
-//         console.log(x)
+//         alert(x)
 //     }
     
 // }
 // AgeCalc()
+
+
+function AgeCalc(){
+
+    let bornYear = document.getElementById("EnterAge").value
+    let x = (2023 - bornYear)
+    results = document.getElementById("Results")
+    results.innerHTML = x
+
+   
+    // let x = (2023 - bornYear)
+
+    if (x <0 ){
+        // alert("Age can not be a negative")
+        results.innerHTML =("Age can not be a negative")
+    } 
+    else if (!bornYear){
+        // alert("Please enter your bornYear")
+        results.innerHTML =("Please enter your bornYear")
+    }
+    else if (bornYear < 0){
+        // alert("Born year can not be less than 0")
+        results.innerHTML =("Born year can not be less than 0")
+    }
+    else {
+        // alert(x)
+        results.innerHTML =(x)
+    }
+
+}
+AgeCalc()
+
