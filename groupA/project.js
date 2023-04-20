0d464ea927eae46c7efc14316b8a0667badb2b23
Aphelele Ndlela <aphelelendlela95@gmail.com>
2023-04-20 17:02:29 +0200

    project

---
+++ groupA/project.js
@@ -0,0 +1,30 @@
+let amount = document.getElementById("App2");
+let x = [];
+let y = `(amount:budget.value)
+`
+
+function add() {
+
+let budget = document.getElementById("App").value;
+
+// let balance = {
+// `
+//     item: 
+//     Date:
+//     amount:
+// `
+// }
+
+
+// budget = amount - items
+
+if(!budget){
+    return("input budget");
+} else {
+    document.querySelector("#item1").innerHTML += budget + " " + amount.value+
+    `<button>Delete</button><button>Edit</button>
+    `
+
+}
+} 
+
